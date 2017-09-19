eade40565be483fd0b10bc71e6a11280c9f7767c
Kay Mok <kaymok3@gmail.com>
2017-09-18 23:55:26 -0400

    Add view method

--- shopping_list.rb
+++ shopping_list.rb
@@ -1,4 +1,7 @@
 # The shopping list helps users keep track of things to be purchased.
+# When the user types "add", the program prompts the user for a list item.
+# When the user enters a list item, the list item gets added to the shopping list.
+# When the user types "view", the program displays a numbered list.
 
 class Item
 
@@ -9,20 +12,21 @@ class Item
 end
 
 class ShoppingList
-
+  attr_accessor :list
   def initialize(name)
     @name = name
-    get_input
-    # @list = list
+    @list = ["apple", "orange", "banana"]
+    get_input()
   end
 
   def get_input
-    puts "To add a shopping list, type 'add'. To view your list, type 'view'. To exit the program, type 'quit'."
+    puts "To add to a shopping list, type 'add'. To view your list, type 'view'. To exit the program, type 'quit'."
     user_input = gets.chomp
     if user_input == "add"
       p "add"
     elsif user_input == "view"
       p "view"
+      view()
     elsif user_input == "quit"
       p "bye"
       return
@@ -43,17 +47,17 @@ class ShoppingList
   #     end
   # end
   #
-  # def view
-  #   puts @list + ": "
-  #   @list.each do |list|
-  #     puts (@list.index.to_i + 1) + ". " + @item
-  #   end
-  #   puts "Anything else?"
-  #   if gets.chomp == "no"
-  #     return @menu
-  #     @list.add_to
-  #   end
-  # end
+  def view
+    puts @name + ": "
+    @list.each_with_index do |item, index|
+      puts (index.to_i + 1).to_s + ". " + item
+    end
+    puts "Anything else?"
+    # If user inputs no, then show instructions again. If users adds item, then add to list.
+    if gets.chomp == "no"
+      get_input()
+    end
+  end
 end
 
 ShoppingList.new("grocery")
