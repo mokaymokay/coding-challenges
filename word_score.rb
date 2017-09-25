813c0dde527b11992445445af3a1300f98e0edf3
Kay Mok <kaymok3@gmail.com>
2017-09-25 18:10:59 -0400

    Add clarification for current functionality

--- word_score.rb
+++ word_score.rb
@@ -6,7 +6,7 @@
 # e.g. word_score("bbb") => 2^3 + 2 => 10
 
 def word_score(word)
-  puts "Enter a word to be scored."
+  puts "Enter a 3-letter English word with no repetition to be scored."
 
   score_table = {"a" => 1, "b" => 2, "c" => 3, "d" => 4, "e" => 5, "f" => 6,
                "g" => 7, "h" => 8, "i" => 9, "j" => 10, "k" => 11, "l" => 12,
@@ -15,13 +15,19 @@ def word_score(word)
                "y" => 25, "z" => 26}
 
   word = gets.chomp.to_s
-  individual = word.split("")
-  p individual[0..(individual.length)]
-  new_array = score_table.fetch_values(individual[0], individual[1], individual[2])
-  p new_array
-  puts "Score of " + word + ": " + new_array.inject(0, :+).to_s
-  # individual.each do |letter|
-  #   puts score_table.fetch_values(individual)
+  # check for duplicates
+  if word.squeeze == word
+    individual = word.split("")
+    p individual[0..(individual.length)]
+    new_array = score_table.fetch_values(individual[0], individual[1], individual[2])
+    p new_array
+    puts "Score of " + word + ": " + new_array.inject(0, :+).to_s
+  else
+    # need to write method for consecutive repetition
+    p word
+  end
+  # individual.each do |element|
+  #   puts score_table.values_at(individual)
   # end
   # individual.each { |letter| puts score_table.fetch_values(individual)}
 end
