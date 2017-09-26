a9a33f1413cd0c6adcd2a4e0b1bf1b72abf731ca
Kay Mok <kaymok3@gmail.com>
2017-09-26 11:25:56 -0400

    Add structure to create method that generates random hex code

---
+++ hex_code_generator.rb
@@ -0,0 +1,14 @@
+# Write a method of any name that generates a random hex code every time it's called.
+
+def hex_number()
+
+  hex_digits = ["0", "1", "2", "3", "4", "5", "6", "7",
+                "8", "9", "A", "B", "C", "D", "E", "F"]
+
+# create empty array called hex_array
+# until hex_array length is 6
+#   generate random character from hex_digits using rand(15) as array index
+#   push to hex_array
+# return hex_array
+
+end
