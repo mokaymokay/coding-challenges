a9a735513cc0f6055497faabd7fd0be25b626a24
Kay Mok <kaymok3@gmail.com>
2017-09-25 22:48:45 -0400

    Refactor score calculation

--- word_score.rb
+++ word_score.rb
@@ -6,7 +6,6 @@
 # e.g. word_score("bbb") => 2^3 + 2 => 10
 
 def word_score(word)
-  puts "Enter an English word with no consecutive repetition to be scored."
 
   score_table = {"a" => 1, "b" => 2, "c" => 3, "d" => 4, "e" => 5, "f" => 6,
                "g" => 7, "h" => 8, "i" => 9, "j" => 10, "k" => 11, "l" => 12,
@@ -14,22 +13,19 @@ def word_score(word)
                "s" => 19, "t" => 20, "u" => 21, "v" => 22, "w" => 23, "x" => 24,
                "y" => 25, "z" => 26}
 
-  word = gets.chomp.to_s
   # check for duplicates
   if word.squeeze == word
     # split into individual strings
     individual = word.split("")
-    array = []
-    # fetch values from hash for each key and push into array
-    individual.each do |element|
-      array.push(score_table.fetch_values(element))
+    score = 0
+    individual.each do |character|
+      score += score_table[character]
     end
-    puts "Score of " + word + ": " + array.flatten.inject(0, :+).to_s
+    puts "Score of " + word + ": " + score.to_s
   else
     # need to write method for consecutive repetition
-    puts "Error"
-    word_score("")
+    return
   end
 end
 
-word_score("")
+word_score("abc")
