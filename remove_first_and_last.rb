2650c280f599ce683b5ce276c46f1d16bdde4c09
Kay Mok <kaymok3@gmail.com>
2017-09-26 12:34:07 -0400

    Add instructions for method to return string after removing first and last characters

---
+++ remove_first_and_last.rb
@@ -0,0 +1,9 @@
+# Write a function that, when given a list of characters as a comma separated
+# string, returns a string containing all except the first and last characters,
+# separated by spaces. If the input string is empty, or the removal of the first
+# and last items would cause the string to be empty, return null value. Arrays
+# are joined by adding a single space between each consecutive array element.
+
+def array(string)
+  #your code here
+end
