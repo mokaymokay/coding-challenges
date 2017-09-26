14de13aa4bff5cb9341bff3dd1479301effe268d
Kay Mok <kaymok3@gmail.com>
2017-09-26 16:34:49 -0400

    Add skeleton for function that spins words with five or more letters

---
+++ spin_words.rb
@@ -0,0 +1,17 @@
+# Write a function that takes in a string of one or more words, and returns the
+# same string, but with all five or more letter words reversed. Strings passed
+# in will consist of only letters and spaces. Spaces will be included only when
+# more than one word is present.
+# spin_words("Hey fellow warriors") => "Hey wollef sroirraw"
+# spin_words("This is a test") => "This is a test"
+# spin_words("This is another test" => "This is rehtona test"
+
+def spin_words(string)
+  # turn string into array if it includes a space
+  #   loop through array to check for words with string size >= 5 and flip them
+  #   return array as string
+  # reverse word if string size >= 5
+  # return word as string
+end
+
+spin_words("Hey fellow warriors")
