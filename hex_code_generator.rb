0b2b0247609bd064d2942114561c5654ba9912f9
Kay Mok <kaymok3@gmail.com>
2017-09-26 11:54:44 -0400

    Fix random number to include last index and refactor

--- hex_code_generator.rb
+++ hex_code_generator.rb
@@ -1,15 +1,15 @@
 # Write a method of any name that generates a random hex code every time it's called.
 
-def hex_number()
+def rand_hex()
 
   hex_system = ["0", "1", "2", "3", "4", "5", "6", "7",
                 "8", "9", "A", "B", "C", "D", "E", "F"]
 
   hex_string = ""
   until hex_string.length == 6
-    hex_string += hex_system.fetch(rand(15))
+    hex_string += hex_system[rand(0..15)]
   end
   puts hex_string
 end
 
-hex_number()
+rand_hex()
