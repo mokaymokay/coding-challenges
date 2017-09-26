fced16f8683c613c1ba19603e17439414b0a1393
Kay Mok <kaymok3@gmail.com>
2017-09-26 18:58:00 -0400

    Add prompt to method to determine the different number out of a set of numbers, based on evenness

---
+++ iq_test.rb
@@ -0,0 +1,9 @@
+# Write a method to find out which one of the given numbers differs from the
+# others based on its evenness. Make sure the format is similiar to that of an
+# IQ test, in which the first number is referred to as "1" and not "0".
+# i.e. iq_test("2 4 7 8 10") => 3
+# i.e. iq_test("1 2 1 1") => 2
+
+def iq_test(numbers)
+  #your code here
+end
