c5871cc6f1fb14acd2f5f15916526bf293f62f2f
Kay Mok <kaymok3@gmail.com>
2017-09-26 17:22:39 -0400

    Add prompt to method that decodes dubstep

---
+++ song_decoder.rb
@@ -0,0 +1,7 @@
+# This method decodes dubstep remix lyrics and restore them to their original form.
+# i.e. song_decoder("WUBWEWUBAREWUBWUBTHEWUBCHAMPIONSWUBMYWUBFRIENDWUB")
+#  =>  WE ARE THE CHAMPIONS MY FRIEND
+
+def song_decoder(song)
+  #TODO
+end
