c96727684ffbc2505398bebc283c744d77453466
Kay Mok <kaymok3@gmail.com>
2017-09-27 16:46:38 -0400

    Add instructions for method to compare two racing objects

---
+++ race.rb
@@ -0,0 +1,11 @@
+# Given two speeds v1 (A speed, integer > 0) and v2 (B speed, integer > 0) and
+# a lead g (integer > 0) how long will it take B to catch A? The result will be
+# an array [h, mn, s] where h, mn, s is the time needed in hours, minutes and
+# seconds (don't worry for fractions of second). If v1 >= v2 then return nil,
+# nothing, null, None or {-1, -1, -1} for C++, C, Go, Nim.
+# e.g. race(720, 850, 70) => [0, 32, 18]
+# e.g. race(80, 91, 37) => [3, 21, 49]
+
+def race(v1, v2, g)
+    # your code
+end
