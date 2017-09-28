af693afc43ecbc00d50c916c730e4e047636a2be
Kay Mok <kaymok3@gmail.com>
2017-09-28 11:02:05 -0400

    Add instructions for method to compare arrays to check if b are the elements in a squared

---
+++ are_they_the_same.rb
@@ -0,0 +1,13 @@
+# Given two arrays a and b write a function comp(a, b) that checks whether the
+# two arrays have the "same" elements, with the same multiplicities. "Same"
+# means, here, that the elements in b are the elements in a squared, regardless
+# of the order.
+# e.g. a = [121, 144, 19, 161, 19, 144, 19, 11]
+# b = [121, 14641, 20736, 361, 25921, 361, 20736, 361]
+# a or b might be nil or null or None. If a or b are nil (or null or None),
+# the problem doesn't make sense so return false.
+# a or b might be []. If a or b are empty the result is evident by itself.
+
+def comp(array1, array2)
+  ...
+end
