a8047163aab4cd09d70dff4e17e336ee88615a70
Kay Mok <kaymok3@gmail.com>
2017-09-28 12:25:16 -0400

    Refactor by using .nil?

--- are_they_the_same.rb
+++ are_they_the_same.rb
@@ -9,14 +9,10 @@
 # a or b might be []. If a or b are empty the result is evident by itself.
 
 def comp(array1, array2)
-  if array1 == nil || array2 == nil
-    false
-  elsif array1.empty? || array2.empty?
-    true
-  else
-    array1.collect { |number| number ** 2}.sort == array2.sort ? true : false
-  end
+  return false if array1.nil? || array2.nil?
+  array1.collect { |number| number ** 2}.sort == array2.sort ? true : false
 end
 
 comp([1, 2, 3], nil)
 comp([121, 144, 19, 161, 19, 144, 19, 11], [121, 14641, 20736, 361, 25921, 361, 20736, 361])
+comp([], [])
