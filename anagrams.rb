406584ce4410fc4ad456aec888e489eaae606f47
Kay Mok <kaymok3@gmail.com>
2017-09-28 18:36:58 -0400

    Add instructions for method to find anagrams of a given word from a given list

---
+++ anagrams.rb
@@ -0,0 +1,11 @@
+# Write a function that will find all the anagrams of a word from a list. Given
+# a word and an array of words, you should return an array of all the anagrams
+# or an empty array if there are none.
+# e.g.:
+# anagrams('abba', ['aabb', 'abcd', 'bbaa', 'dada']) => ['aabb', 'bbaa']
+# anagrams('racer', ['crazer', 'carer', 'racar', 'caers', 'racer']) => ['carer', 'racer']
+# anagrams('laser', ['lazing', 'lazy',  'lacer']) => []
+
+def anagrams(word, words)
+  #TODO
+end
