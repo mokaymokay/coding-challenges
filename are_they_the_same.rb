638b8656a9db845c0575a009fb3e306a87403c75
Kay Mok <kaymok3@gmail.com>
2017-09-28 12:21:17 -0400

    Add method

--- are_they_the_same.rb
+++ are_they_the_same.rb
@@ -9,5 +9,14 @@
 # a or b might be []. If a or b are empty the result is evident by itself.
 
 def comp(array1, array2)
-  ...
+  if array1 == nil || array2 == nil
+    false
+  elsif array1.empty? || array2.empty?
+    true
+  else
+    array1.collect { |number| number ** 2}.sort == array2.sort ? true : false
+  end
 end
+
+comp([1, 2, 3], nil)
+comp([121, 144, 19, 161, 19, 144, 19, 11], [121, 14641, 20736, 361, 25921, 361, 20736, 361])
