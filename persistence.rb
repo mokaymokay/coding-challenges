d0dbdccc19b2dce6fbc9832b1f7fee89935ee5a8
Kay Mok <kaymok3@gmail.com>
2017-09-29 16:04:13 -0400

    Add instructions for method to count the number of times it takes to multiply the digits of a number until it becomes a single-digit

---
+++ persistence.rb
@@ -0,0 +1,10 @@
+# Write a function, persistence, that takes in a positive parameter num and
+# returns its multiplicative persistence, which is the number of times you must
+# multiply the digits in num until you reach a single digit.
+# e.g. persistence(39) => 3 ; because 3*9=27, 2*7=14, 1*4=4 and 4 has only one digit
+# e.g. persistence(999) => 4 ; because 9*9*9=729, 7*2*9=126, 1*2*6=12, 1*2=2
+# e.g. persistence(4) => 0 ; because 4 is already a one-digit number
+
+def persistence(n)
+    # your code
+end
