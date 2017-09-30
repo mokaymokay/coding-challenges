92d45d3d7930eb9202e87ada41e0ee05f8d35809
Kay Mok <kaymok3@gmail.com>
2017-09-30 11:42:02 -0400

    Add instructions for method to order a list of numbers by the sum of its digits

---
+++ order_weights.rb
@@ -0,0 +1,17 @@
+# Write a method to order a list of club members' weights by the sum of its
+# digits, i.e. 99 will have "weight" 18, 100 will have weight "1", so in the list,
+# 100 comes before 99.
+#
+# "56 65 74 100 99 68 86 180 90" ordered by numbers weights
+# becomes: "100 180 90 56 65 74 68 86 99".
+#
+# When two numbers have the same "weight", class them as if they were strings
+# and not numbers: 100 is before 180 because its "weight" (1) is less than the
+# one of 180 (9) and 180 is before 90 since, having the same "weight" (9) it
+# comes before as a string.
+#
+# All numbers in the list are positive numbers and the list can be empty.
+
+def order_weights(array)
+  #TODO
+end
