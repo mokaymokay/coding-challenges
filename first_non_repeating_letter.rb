8429519673f9b4d352c719bb0d16291efbe2e73d
Kay Mok <kaymok3@gmail.com>
2017-10-11 13:53:39 -0500

    Add instructions for method to output first non-repeating character in a string

---
+++ first_non_repeating_letter.rb
@@ -0,0 +1,11 @@
+# Write a function named first_non_repeating_letter that takes a string input,
+# and returns the first character that is not repeated anywhere in the string.
+# i.e. 'stress' => 't'
+# As an added challenge, upper- and lowercase letters are considered the same
+# character, but the function should return the correct case for the initial letter.
+# i.e. 'sTreSS' => 'T'
+# If a string contains all repeating characters, it should return the empty string ("").
+
+def  first_non_repeating_letter(s)
+  #
+end
