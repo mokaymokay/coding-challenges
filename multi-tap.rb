0bde6d9004f418727347453dacca27dd30c2817d
Kay Mok <kaymok3@gmail.com>
2017-11-21 14:10:10 -0500

    Add instructions for method to calculate number of key presses needed to type a phrase on an old phone using multi-tap

---
+++ multi-tap.rb
@@ -0,0 +1,11 @@
+# This module calculates the amount of key presses required for a phrase on a
+# phone with a 3x4 numeric keypad using multi-tap (pressing a button repeatedly
+# to cycle through possible values), i.e. "E" can be typed by pressing 3 twice.
+# presses("WHERE DO U WANT 2 MEET L8R") => 47
+
+def presses(phrase)
+#code
+end
+
+presses("LOL")
+presses("HOW R U")
