067d9ad002211c041aed2829e6488b88088371ef
Kay Mok <kaymok3@gmail.com>
2017-11-24 12:35:35 -0500

    Add instructions for method to autocomplete an input using a given array of words

---
+++ autocomplete.rb
@@ -0,0 +1,9 @@
+# This method takes in an input string and a dictionary array and return the values
+# that begin with that input string. If there are more than 5 matches, output will
+# be limited to the first 5 results. It there are no matches, an empty array will
+# be returned.
+# i.e. autocomplete('ai', ['airplane','airport','apple','ball']) = ['airplane','airport']
+
+def autocomplete(input, dictionary)
+  #your code here
+end
