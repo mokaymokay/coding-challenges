a3ebe8503c24546b8e42c30ddaf1c5d0db213272
Kay Mok <kaymok3@gmail.com>
2018-01-05 13:29:14 -0500

    Add instructions for method to output the sum of individual digits of a number

---
+++ sum_of_digits.js
@@ -0,0 +1,14 @@
+/*
+JavaScript JumpStart Online: Sum of Digits
+
+Create the function sumOfDigits that adds individual digits of a number, and
+returns the sum.
+
+**Examples**
+
+- INPUT: sumOfDigits(414);
+- OUTPUT: 9
+
+- INPUT: sumOfDigits(2913);
+- OUTPUT: 15
+*/
