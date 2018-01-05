a22c12495f57dbc4998d95395bf69431293c08bd
Kay Mok <kaymok3@gmail.com>
2018-01-04 21:27:50 -0500

    Add instructions for method to return the number of vowels contained in a given string

---
+++ vowel_count.js
@@ -0,0 +1,17 @@
+/*
+JavaScript JumpStart Online: Vowel Count
+
+Create the function vowelCount(str) that takes a str parameter and returns the
+number of vowels the string contains (ie. "Fullstack Academy" would return 5).
+
+Do not count 'y' as a vowel for this challenge.
+
+**Examples**
+
+- INPUT: vowelCount("Good Job");
+- OUTPUT: 3
+
+
+- INPUT: vowelCount('Yankees');
+- OUTPUT: 3
+*/
