f4572367c76a5ac10f240a5387668a3a8de40695
Kay Mok <kaymok3@gmail.com>
2018-01-05 17:19:22 -0500

    Add instructions for method to return the factorial of a given number

---
+++ factorial.js
@@ -0,0 +1,15 @@
+/*
+JavaScript JumpStart Online: First Factorial
+
+Create the function factorial(num) that returns the factorial of the parameter.
+The factorial of n is denoted by n! and calculated by the product of integer
+numbers from 1 to n. For n > 0, n! = 1*2*3*...*n. For n = 0, 0! = 1.
+
+**Examples**
+
+- INPUT: factorial(4);
+- OUTPUT: 24
+
+- INPUT: factorial(5);
+- OUTPUT: 120
+*/
