debe7ed87dd1cc31bb4527cc4ab489edbad3204b
Kay Mok <kaymok3@gmail.com>
2018-01-05 13:03:44 -0500

    Add instructions for method to reverse a string

---
+++ reverse_a_string.js
@@ -0,0 +1,13 @@
+/*
+JavaScript JumpStart Online: Reverse a String
+
+Create the function "reverseString" that reverses a string, and returns the
+value of the reversed string.
+
+NOTE: Do not use the .reverse() method
+
+**Examples**
+
+- INPUT: reverseString("hello");
+- OUTPUT: 'olleh'
+*/
