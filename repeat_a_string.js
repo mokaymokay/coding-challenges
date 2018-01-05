11d0929f825bea4b0d1db49f996f14ba1dc28b8e
Kay Mok <kaymok3@gmail.com>
2018-01-04 21:20:12 -0500

    Add instructions for method to repeat string for a given number of times

---
+++ repeat_a_string.js
@@ -0,0 +1,21 @@
+/*
+JavaScript JumpStart Online: Repeat a String
+
+Create a function "repeat" that accepts two arguments, the string to repeat and
+the a number that represtents how many times to repeat the string argument.
+Return a string that concatenates all the times the string is repeated.
+
+*NOTE: Do NOT use the [String.prototype.repeat](https://developer.mozilla.orgen-
+       US/docs/Web/JavaScript/Reference/Global_Objects/String/repeat)*
+
+**Examples**
+
+- INPUT: repeat("yo", 4);
+- OUTPUT: 'yoyoyoyo'
+
+- INPUT: repeat("yo", 0);
+- OUTPUT: ''
+
+- INPUT: repeat("bye", 6);
+- OUTPUT: 'byebyebyebyebyebye'
+*/
