d421593bc5460c22525d2424577a871b00410f44
Kay Mok <kaymok3@gmail.com>
2018-01-05 20:03:01 -0500

    Add instructions for method to convert a variable name from under_score to camcelCase

---
+++ underscore_to_camel_case.js
@@ -0,0 +1,15 @@
+/*
+JavaScript JumpStart Online: Underscore To Camel Case
+
+Write a function to convert a variable name from under_score format to camelCase.
+
+Make sure you support an unlimited number of underscores in the input!
+
+**Examples**
+
+- INPUT: underToCamel("first_name");
+- OUTPUT: "firstName"
+
+- INPUT: underToCamel("my_income_tax_from_2015");
+- OUTPUT: "myIncomeTaxFrom2015"
+*/
