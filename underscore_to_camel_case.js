b1ec15da6cbee85c5ec6df9c16a39090ce94698d
Kay Mok <kaymok3@gmail.com>
2018-01-05 21:38:26 -0500

    Add method

--- underscore_to_camel_case.js
+++ underscore_to_camel_case.js
@@ -13,3 +13,25 @@ Make sure you support an unlimited number of underscores in the input!
 - INPUT: underToCamel("my_income_tax_from_2015");
 - OUTPUT: "myIncomeTaxFrom2015"
 */
+
+function underToCamel(str){
+  var output = "",
+      found_underscore = false;
+
+  for(var i = 0; i < str.length ; i++){
+    if(str[i] === "_"){
+      found_underscore = true;
+    } else {
+      if(found_underscore){
+        output += str[i].toUpperCase();
+        found_underscore = false;
+      } else {
+        output += str[i];
+      }
+    }
+  }
+  return output;
+}
+
+underToCamel("first_name");
+underToCamel("my_income_tax_from_2015");
