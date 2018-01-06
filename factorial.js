226bd3ffc108beba976549a3453f573732c56ce7
Kay Mok <kaymok3@gmail.com>
2018-01-05 19:58:56 -0500

    Add method

--- factorial.js
+++ factorial.js
@@ -13,3 +13,15 @@ numbers from 1 to n. For n > 0, n! = 1*2*3*...*n. For n = 0, 0! = 1.
 - INPUT: factorial(5);
 - OUTPUT: 120
 */
+
+function factorial(n) {
+  var output = 1;
+  for(n; n > 0; n--){
+    output *= n;
+  }
+  return output;
+}
+
+factorial(1);
+factorial(4);
+factorial(5);
