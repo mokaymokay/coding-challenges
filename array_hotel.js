e653a854a4c00ba5f4fa6059e77ff9b80f2107de
Kay Mok <kaymok3@gmail.com>
2018-01-06 16:37:19 -0500

    Add instructions for methods to (1) find vacancies in the hotel array and (2) find the cheapest vacant room

---
+++ array_hotel.js
@@ -0,0 +1,43 @@
+/*
+JavaScript JumpStart Online: The Array Hotel
+
+CHALLENGE 1: FIND VACANT ROOMS
+
+In this challenge, we'll be using an array to manage the occupancy of a hotel
+called The Array Hotel.
+
+Each room in the hotel will be represented with a "vacant" or "occupied" string
+in the array. The index of the string in the array is the room number.
+
+For example, for a hotel with an occupancy array of ["occupied", "vacant",
+"occupied", "vacant"], we can determine that Rooms 0 and 2 are occupied and
+Rooms 1 and 3 are free.
+
+Your job is to write a function called findVacantRooms to help the owner of the
+hotel find which rooms are vacant. This function should return an array of the
+room numbers that are vacant.
+
+**Examples**
+
+- INPUT: findVacantRooms(["vacant", "occupied", "occupied", "vacant", "vacant"]);
+- OUTPUT: [0,3,4] since those are the vacant rooms
+*/
+
+// code here
+
+/*
+CHALLENGE 2: FIND THE CHEAPEST VACANT ROOM
+
+Assume now that instead of just "vacant" and "occupied", the occupancy array has
+the price of the room listed for the vacant rooms. For example, an example
+occupancy array could be as follows: [112, "occupied", 150, 175, "occupied"].
+
+This would mean that Rooms 0, 2 and 3 are available for stay at prices $112,
+$150 and $175 for the night.
+
+Make a function to help someone find the cheapest available room in the hotel.
+Return the room number and its​ price.
+
+*/
+
+// code here
