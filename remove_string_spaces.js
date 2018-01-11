20b90df549f48391e669ec864c410702315f7843
Kay Mok <kaymok3@gmail.com>
2018-01-11 12:31:48 -0500

    Add method to remove spaces from a given string

---
+++ remove_string_spaces.js
@@ -0,0 +1,8 @@
+// Simple, remove the spaces from the string, then return the resultant string.
+
+function noSpace(str) {
+  return str.replace(/\s/g, "");
+}
+
+noSpace("r u b y");
+noSpace("    ja    v   asc   ript ");
