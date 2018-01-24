0642b95f41e8601411a4f6d7e3a0deca1e57d1ce
Kay Mok <kaymok3@gmail.com>
2018-01-24 16:48:37 -0500

    Add method

--- javascript/fullstackjack_card_game.js
+++ javascript/fullstackjack_card_game.js
@@ -29,5 +29,33 @@ fullstackJack(['A', '7', '8'], ['K', '5', '9']);
 // --> returns "Player 1 wins 2 to 1!"
 
 fullstackJack(['K', 'Q', 'J'], ['Q', 'K', 'J'])
-// --> returns "Player 1 and 2 Tie!"
+// --> returns "Tie!"
 */
+
+function fullstackJack(player1Hand, player2Hand) {
+  var cardRank = ['2','3','4','5','6','7','8','9','T','J','Q','K','A'],
+      player1Score = 0,
+      player2Score = 0,
+      i = 0;
+
+  while (i < player1Hand.length ) {
+    if (cardRank.indexOf(player1Hand[i]) > cardRank.indexOf(player2Hand[i])) {
+      player1Score += 1;
+    } else if (cardRank.indexOf(player1Hand[i]) < cardRank.indexOf(player2Hand[i])) {
+      player2Score += 1;
+    }
+    i++;
+  }
+
+  if (player1Score > player2Score) {
+    return "Player 1 wins " + player1Score + " to " + player2Score + "!";
+  } else if (player1Score < player2Score) {
+    return "Player 2 wins " + player2Score + " to " + player1Score + "!";
+  } else {
+    return "Tie!";
+  }
+}
+
+// fullstackJack(['A', '7', '8'], ['K', '5', '9']);
+// fullstackJack(['K', 'Q', 'J'], ['Q', 'K', 'J']);
+// fullstackJack(['K', '5', '2', 'T'], ['A', 'J', 'Q', '8']);
