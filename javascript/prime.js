40fb89d13334f76af65a407898b10dbb81567efe
Kay Mok <kaymok3@gmail.com>
2018-01-26 09:31:17 -0500

    Add instructions for method to determine if given number is prime

---
+++ javascript/prime.js
@@ -0,0 +1,15 @@
+/*
+JavaScript JumpStart Online: Prime
+
+The function prime(num) accepts a number argument and returns the boolean true
+if the number is a prime number, otherwise it returns false.
+
+**Examples**
+
+- INPUT: prime(5);
+- OUTPUT: true
+
+- INPUT: prime(10);
+- OUTPUT: false
+
+*/
