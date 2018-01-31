f0ef846436cbb5ab3c677e38ad41816215e895bf
Kay Mok <kaymok3@gmail.com>
2018-01-31 11:30:06 -0500

    Add method

--- javascript/prime.js
+++ javascript/prime.js
@@ -13,3 +13,21 @@ if the number is a prime number, otherwise it returns false.
 - OUTPUT: false
 
 */
+
+function prime(num) {
+  var factors = [];
+
+  for (var i = 1; i <= num; i++) {
+    if (num % i === 0) {
+      factors.push(i);
+    }
+  }
+
+  if (factors.length <= 2) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+prime(11);
