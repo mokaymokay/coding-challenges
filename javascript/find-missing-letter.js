7256b1b1d277891c243846944eb0eccce962f099
Kay Mok <kaymok3@gmail.com>
2018-02-10 15:06:31 -0500

    Add instructions for method to determine the missing letter in an array of consecutive letters

---
+++ javascript/find-missing-letter.js
@@ -0,0 +1,18 @@
+/*
+Write a method that takes an array of consecutive (increasing) letters as input
+and that returns the missing letter in the array.
+
+You will always get an valid array, and it will be always exactly one letter be
+missing. The length of the array will always be at least 2.
+
+The array will always contain letters in only one case.
+
+**Examples**
+
+- INPUT: ['a','b','c','d','f']
+- OUTPUT: 'e'
+
+- INPUT: ['O','Q','R','S']
+- OUTPUT: 'P'
+
+*/
