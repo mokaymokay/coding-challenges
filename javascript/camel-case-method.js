b350f8ec9ecda4a8ce9915f044604111489d94ea
Kay Mok <kaymok3@gmail.com>
2018-02-10 18:50:19 -0500

    Add instructions for method to take a string and return its words with the first letter capitalized without spaces

---
+++ javascript/camel-case-method.js
@@ -0,0 +1,11 @@
+/*
+Write simple .camelCase method for strings. All words must have their first
+letter capitalized without spaces.
+
+**Examples**
+
+- INPUT: "hello case".camelCase()
+- OUTPUT: HelloCase
+
+- INPUT: "camel case word".camelCase()
+- OUTPUT: CamelCaseWord
