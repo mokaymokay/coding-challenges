b1eed4490f417a9789ca42da47923b04db2adc09
Kay Mok <kaymok3@gmail.com>
2018-02-10 18:11:35 -0500

    Add method

--- javascript/find-missing-letter.js
+++ javascript/find-missing-letter.js
@@ -16,3 +16,21 @@ The array will always contain letters in only one case.
 - OUTPUT: 'P'
 
 */
+
+function findMissingLetter(input) {
+  const alphabet = ['a','b','c','d','e','f','g','h','i','j','k','l','m',
+                    'n','o','p','q','r','s','t','u','v','w','x','y','z'];
+
+  const isLowerCase = input[0] === input[0].toLowerCase();
+  input = input.map(letter => letter.toLowerCase());
+  var startingLetterIndex = alphabet.indexOf(input[0]);
+  newArray = [];
+  for (i = 0; i < (input.length + 1); i++) {
+    newArray.push(alphabet[startingLetterIndex + i]);
+  }
+  output = newArray.filter(x => !input.includes(x)).toString();
+  return isLowerCase ? output : output.toUpperCase();
+}
+
+findMissingLetter(['a','b','c','d','f']);
+findMissingLetter(['O','Q','R','S']);
