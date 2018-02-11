8098dae4dd8f744258f2c5eb4b1fe1e52941437b
Kay Mok <kaymok3@gmail.com>
2018-02-10 19:21:08 -0500

    Add method

--- javascript/camel-case-method.js
+++ javascript/camel-case-method.js
@@ -9,3 +9,13 @@ letter capitalized without spaces.
 
 - INPUT: "camel case word".camelCase()
 - OUTPUT: CamelCaseWord
+
+*/
+
+String.prototype.camelCase = function() {
+  return this.split(' ').map(function(word){
+    return word.charAt(0).toUpperCase() + word.slice(1);
+  }).join('');
+}
+
+"hello case".camelCase();
