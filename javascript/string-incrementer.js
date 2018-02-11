8a6572cd0c5f9af0aa1a3190c9ff6158222d1938
Kay Mok <kaymok3@gmail.com>
2018-02-11 17:01:03 -0500

    Add method

--- javascript/string-incrementer.js
+++ javascript/string-incrementer.js
@@ -1,7 +1,7 @@
 /*
 Your job is to write a function which increments a string, to create a new string.
-If the string already ends with a number, the number should be incremented by 1.
-If the string does not end with a number the number 1 should be appended to the
+If the string already ends with a num, the num should be incremented by 1.
+If the string does not end with a num the num 1 should be appended to the
 new string.
 
 **Examples**
@@ -21,3 +21,27 @@ foo099 -> foo100
 Attention: If the number has leading zeros the amount of digits should be considered.
 
 */
+
+function incrementString(str) {
+  var output = "",
+      startingDigit = str.search(/\d/);
+  if (startingDigit !== -1) {
+    var num = str.slice(startingDigit);
+    output += str.slice(0, startingDigit);
+    var added = (parseInt(num) + 1) + "";
+    for (var i = 0; added.length < num.length; i++) {
+      added = "0" * i + added;
+    }
+    output += added;
+  } else {
+    output = str + 1;
+  }
+  return output;
+}
+
+incrementString("foo");
+incrementString("foobar23");
+incrementString("foo0042");
+incrementString("foo9");
+incrementString("foo099");
+incrementString("");
