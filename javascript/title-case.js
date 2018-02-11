e84bdbe7cf4193c6bcb4a8216c20e20d01d7c5cf
Kay Mok <kaymok3@gmail.com>
2018-02-11 17:51:58 -0500

    Add instructions for method to convert a string into title case

---
+++ javascript/title-case.js
@@ -0,0 +1,25 @@
+/*
+Write a function that will convert a string into title case, given an optional
+list of exceptions (minor words). The list of minor words will be given as a
+string with each word separated by a space. Your function should ignore the case
+of the minor words string -- it should behave in the same way even if the case of
+the minor word string is changed.
+
+First argument (required): the original string to be converted.
+
+Second argument (optional): space-delimited list of minor words that must always
+be lowercase except for the first word in the string. The JavaScript/CoffeeScript
+tests will pass undefined when this argument is unused.
+
+**Examples**
+
+- INPUT: 'a clash of KINGS', 'a an the of'
+- OUTPUT: 'A Clash of Kings'
+
+- INPUT: THE WIND IN THE WILLOWS', 'The In', 'a an the of'
+- OUTPUT: 'The Wind in the Willows'
+
+- INPUT: 'the quick brown fox'
+- OUTPUT: 'The Quick Brown Fox'
+
+*/
