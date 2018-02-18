a0a16bb15a55b0a7bf459ccb5008795020a2f806
Kay Mok <kaymok3@gmail.com>
2018-02-17 20:22:05 -0500

    Add function

--- javascript/recursion-evenness.js
+++ javascript/recursion-evenness.js
@@ -9,3 +9,19 @@ and return a Boolean. Test it on 50 and 75. See how it behaves on -1, and try to
 fix it.
 
 */
+
+function isEven(num) {
+  if (num === 0) {
+    return true;
+  } else if (num === 1) {
+    return false;
+  } else if (num < 0) {
+    return isEven(-num);
+  } else {
+    return isEven (num - 2);
+  }
+}
+
+console.log(isEven(50)); // returns true
+console.log(isEven(75)); // returns false
+console.log(isEven(-1)); // returns false
