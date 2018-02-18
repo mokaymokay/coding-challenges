7709994032837f0a9557f1fc805694ee6014e74a
Kay Mok <kaymok3@gmail.com>
2018-02-18 14:17:43 -0500

    Add second method to count occurences of given character in given string

--- javascript/bean-counting.js
+++ javascript/bean-counting.js
@@ -25,3 +25,15 @@ function countBs(string) {
 }
 
 console.log(countBs("BbbBbBB"));
+
+function countChar(string, char) {
+  let count = 0;
+  for (let i = 0; i < string.length; i++) {
+    if (string[i] === char) {
+      count += 1;
+    }
+  }
+  return count;
+}
+
+console.log(countChar("wingardium leviosa", "i"));
