14fec936016ce4276126b66d021596301612af35
Kay Mok <kaymok3@gmail.com>
2018-02-18 14:23:06 -0500

    Refactor

--- javascript/bean-counting.js
+++ javascript/bean-counting.js
@@ -24,7 +24,7 @@ function countChar(string, char) {
   let count = 0;
   for (let i = 0; i < string.length; i++) {
     if (string[i] === char) {
-      count += 1;
+      count ++;
     }
   }
   return count;
