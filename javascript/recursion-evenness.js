e6243e6344627f8444e1eb081e28485b6f17bbed
Kay Mok <kaymok3@gmail.com>
2018-02-17 20:15:32 -0500

    Add instructions for a recursive function to test whether or not a number is even

---
+++ javascript/recursion-evenness.js
@@ -0,0 +1,11 @@
+/*
+The remainder operator, %, can be used to test whether a number is even or odd by
+using % 2 to see whether it’s divisible by two.
+
+Define a recursive function isEven corresponding to these descriptions: a) Zero
+is even, b) One is odd, c) for any other number N, its evenness is the same as
+N - 2.. The function should accept a single parameter (a positive, whole number)
+and return a Boolean. Test it on 50 and 75. See how it behaves on -1, and try to
+fix it.
+
+*/
