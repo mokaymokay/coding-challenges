476ed04b2dcb021d7366b419416f03891691c3ea
Kay Mok <kaymok3@gmail.com>
2018-02-19 23:27:56 -0500

    Add function

--- javascript/who-likes-it.js
+++ javascript/who-likes-it.js
@@ -15,3 +15,25 @@ likes(["Alex", "Jacob", "Mark", "Max"]) --> "Alex, Jacob and 2 others like this"
 
 For more than 4 names, the number in "and 2 others" simply increases.
 */
+
+function likes(names) {
+  let string = "";
+  if (names.length == 0) {
+    string += "no one likes this";
+  } else if (names.length == 1) {
+    string += names + " likes this";
+  } else if (names.length == 2) {
+    string += names[0] + " and " + names[1] + " like this";
+  } else if (names.length == 3) {
+    string += names[0] + ", " + names[1] + " and " + names[2] + " like this";
+  } else {
+    string += names[0] + ", " + names[1] + " and " + (names.length - 2) + " others like this"
+  }
+  return string;
+}
+
+console.log(likes([])); // no one likes this
+console.log(likes(["Peter"])); // Peter likes this
+console.log(likes(["Jacob", "Alex"])); // Jacob and Alex like this
+console.log(likes(["Max", "John", "Mark"])); // Max, John and Mark like this
+console.log(likes(["Alex", "Jacob", "Mark", "Max"])); // Alex, Jacob and 2 others like this
