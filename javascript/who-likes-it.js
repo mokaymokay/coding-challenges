41a1be82f4e88626a77f9edf3577b01f4c312b7c
Kay Mok <kaymok3@gmail.com>
2018-02-20 23:22:07 -0500

    Refactor

--- javascript/who-likes-it.js
+++ javascript/who-likes-it.js
@@ -17,19 +17,13 @@ For more than 4 names, the number in "and 2 others" simply increases.
 */
 
 function likes(names) {
-  let string = "";
-  if (names.length == 0) {
-    string += "no one likes this";
-  } else if (names.length == 1) {
-    string += names + " likes this";
-  } else if (names.length == 2) {
-    string += names[0] + " and " + names[1] + " like this";
-  } else if (names.length == 3) {
-    string += names[0] + ", " + names[1] + " and " + names[2] + " like this";
-  } else {
-    string += names[0] + ", " + names[1] + " and " + (names.length - 2) + " others like this"
+  switch(names.length) {
+    case 0: return "no one likes this"; break;
+    case 1: return names + " likes this"; break;
+    case 2: return names[0] + " and " + names[1] + " like this"; break;
+    case 3: return names[0] + ", " + names[1] + " and " + names[2] + " like this"; break;
+    default: return names[0] + ", " + names[1] + " and " + (names.length - 2) + " others like this";
   }
-  return string;
 }
 
 console.log(likes([])); // no one likes this
