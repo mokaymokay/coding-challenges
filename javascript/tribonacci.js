45791eadeb5a0e23f4268b2f62dba4737d9bb535
Kay Mok <kaymok3@gmail.com>
2018-03-04 14:28:21 -0500

    Add function

--- javascript/tribonacci.js
+++ javascript/tribonacci.js
@@ -18,3 +18,23 @@ If n < 3, return the signature array.
 - OUTPUT: [1,2,3,6,11,20,37,68,125,230]
 
 */
+
+function tribonacci(signature, n) {
+  if (signature.length < 3) return signature;
+  if (signature.length > n) {
+    while (signature.length > n) {
+      signature.pop();
+    }
+  }
+  while (signature.length < n) {
+    let i = signature.length - 1;
+    signature.push(signature[i] + signature[i-1] + signature[i-2]);
+  }
+  return signature;
+}
+
+console.log(tribonacci([1,1], 10));
+console.log(tribonacci([0,0,1], 10));
+console.log(tribonacci([1,2,3], 10));
+console.log(tribonacci([100,200,300], 0));
+console.log(tribonacci([1,1,1], 1));
