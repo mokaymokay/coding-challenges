b06a299a92667aed308db7e35b6c5b380a6bd55a
Kay Mok <kaymok3@gmail.com>
2018-03-04 20:41:27 -0500

    Add instructions for function that takes a signature of x elements, adds next elements by summing the last x elements until it has the length of n, then returns the sequence

---
+++ javascript/xbonacci.js
@@ -0,0 +1,19 @@
+/*
+Write a Xbonacci function that takes a signature of x elements, adds next elements
+by summing the last x elements, until the seeded sequence has the length of n.
+
+**Examples**
+
+- INPUT: xbonacci([1,1,1,1], 10)
+- OUTPUT: [1,1,1,1,4,7,13,25,49,94]
+
+- INPUT: xbonacci([0,0,0,0,1], 10)
+- OUTPUT: [0,0,0,0,1,1,2,4,8,16]
+
+- INPUT: xbonacci([1,0,0,0,0,0,1], 10)
+- OUTPUT: [1,0,0,0,0,0,1,2,3,6]
+
+- INPUT: xbonacci([1,1], 10)
+- OUTPUT: [1,1,2,3,5,8,13,21,34,55] (Fibonacci Sequence)
+
+*/
