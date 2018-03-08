239e2992868f507355774c8b7fbe90328dfae673
Kay Mok <kaymok3@gmail.com>
2018-03-08 16:31:10 -0500

    Add function

--- javascript/movie-selector.js
+++ javascript/movie-selector.js
@@ -16,3 +16,27 @@ When building your method:
 2. Don't make your users watch the same movie twice
 
 */
+
+// loop through entire array using index
+// repeat process of adding 2 numbers together to check if equal to flightLength
+
+function movieFlightChecker(movieLengths, flightLength) {
+  // for loop through every element of array using index
+  for (let i = 0; i < movieLengths.length; i++) {
+    // at current index, make variable of firstMovie
+    let firstMovie = movieLengths[i];
+    // another for loop to loop through every element of array using index
+    for (let j = i + 1; j < movieLengths.length; j++) {
+      // at current index, make variable of secondMovie
+      let secondMovie = movieLengths[j];
+      // check if sum of firstMovie and secondMovie equals flightLength
+      if (firstMovie + secondMovie === flightLength) return true;
+      }
+    }
+  return false;
+}
+
+let movieLengths = [70,120,230,93,89,45,168,345]
+let flightLength = 165
+
+console.log(movieFlightChecker(movieLengths, flightLength)); //returns true
