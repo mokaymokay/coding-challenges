524a7616829ac6d5f8d9d20081e1185f7d10ea47
Kay Mok <kaymok3@gmail.com>
2018-03-08 21:09:53 -0500

    Add instructions for function to take in a sentence or paragraph and return the Pig Latin equivalent

---
+++ javascript/piglatinizer.js
@@ -0,0 +1,11 @@
+/*
+
+Pig Latin is a game of alterations played on the English language. To create the
+Pig Latin form of an English word the initial consonant sound is transposed to the
+end of the word and an ay is affixed (Ex.: "banana" would yield anana-bay). Check
+out Wikipedia for more information on rules - try to get as close as possible.
+
+Compose a JavaScript function that takes in a regular sentence or paragraph and
+returns the Pig Latin equivalent.
+
+*/
