3ac075d6d8e488a077af6c0e3232e1b7e0fc2f00
Kay Mok <kaymok3@gmail.com>
2018-03-08 19:33:30 -0500

    Add method

--- javascript/merge-arrays.js
+++ javascript/merge-arrays.js
@@ -16,3 +16,27 @@ console.log(mergeArrays(myArray, alicesArray));
 // logs [1, 3, 4, 5, 6, 8, 10, 11, 12, 14, 15, 19]
 
 */
+
+var myArray     = [3, 4, 6, 10, 11, 20];
+var alicesArray = [1, 5, 8, 12, 14, 19];
+
+function mergeArrays(myArray, alicesArray) {
+  var newArray = [],
+      m = 0,
+      a = 0;
+
+while (newArray.length < myArray.length + alicesArray.length) {
+    if (myArray[m] < alicesArray[a] || a === alicesArray.length) {
+      newArray.push(myArray[m]);
+      m++;
+      console.log("m:" + m);
+    } else {
+      newArray.push(alicesArray[a]);
+      a++;
+      console.log("a:" + a);
+    }
+  }
+  return newArray;
+}
+
+console.log(mergeArrays(myArray, alicesArray));
