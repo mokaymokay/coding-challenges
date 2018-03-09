255297ce5ef31c04c34dadcc9eb86db3f17687f7
Kay Mok <kaymok3@gmail.com>
2018-03-08 23:08:51 -0500

    Add function to turn word into Pig Latin if it begins with a vowel

--- javascript/piglatinizer.js
+++ javascript/piglatinizer.js
@@ -1,5 +1,4 @@
 /*
-
 Pig Latin is a game of alterations played on the English language. To create the
 Pig Latin form of an English word the initial consonant sound is transposed to the
 end of the word and an ay is affixed (Ex.: "banana" would yield anana-bay). Check
@@ -7,5 +6,13 @@ out Wikipedia for more information on rules - try to get as close as possible.
 
 Compose a JavaScript function that takes in a regular sentence or paragraph and
 returns the Pig Latin equivalent.
-
 */
+
+let piglatinizer = (word) => {
+  if (word.search(/[aeiou]/gi) === 0) {
+    word += "way";
+    console.log(word);
+  }
+}
+
+piglatinizer("egg");
