7f4f7659d73b24b3ebb7fc60888e671b1d56706c
Kay Mok <kaymok3@gmail.com>
2018-03-08 20:25:51 -0500

    Add function

--- javascript/fizzbuzz.js
+++ javascript/fizzbuzz.js
@@ -3,3 +3,19 @@ Write a program that console.logs the numbers from 1 to 100. But for multiples o
 three print “Fizz” instead of the number and for the multiples of five console.log
 “Buzz”. For numbers which are multiples of both three and five console.log “FizzBuzz”.
 */
+
+let fizzbuzzer = () => {
+  for (let i = 1; i < 100; i++) {
+    if (i % 3 === 0 && i % 5 === 0) {
+      console.log("FizzBuzz");
+    } else if (i % 3 === 0) {
+      console.log("Fizz");
+    } else if (i % 5 === 0) {
+      console.log("Buzz");
+    } else {
+      console.log(i);
+    }
+  }
+}
+
+fizzbuzzer();
