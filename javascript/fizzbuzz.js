9c40298ebb7f2e123cd3b4e26791399c9ba2ecf7
Kay Mok <kaymok3@gmail.com>
2018-03-09 11:55:40 -0500

    Fix function to include the 100th iteration

--- javascript/fizzbuzz.js
+++ javascript/fizzbuzz.js
@@ -5,7 +5,7 @@ three print “Fizz” instead of the number and for the multiples of five conso
 */
 
 let fizzbuzzer = () => {
-  for (let i = 1; i < 100; i++) {
+  for (let i = 1; i <= 100; i++) {
     if (i % 3 === 0 && i % 5 === 0) {
       console.log("FizzBuzz");
     } else if (i % 3 === 0) {
