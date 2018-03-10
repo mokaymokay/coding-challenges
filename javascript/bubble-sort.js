233452dfb55bf72ad93d4db61ab80a7286c58602
Kay Mok <kaymok3@gmail.com>
2018-03-10 17:20:07 -0500

    Add my definition of the bubble sort algorithm

---
+++ javascript/bubble-sort.js
@@ -0,0 +1,5 @@
+/*
+The bubble sort is a sorting algorithm that iterates through each element of the
+array multple times, comparing two adjacent elements at a time and swapping their
+positions accordingly.
+*/
