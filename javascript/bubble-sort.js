083361d036b6d2e3ab4d4345c98e32b44c31fa5a
Kay Mok <kaymok3@gmail.com>
2018-03-10 21:40:23 -0500

    Add bubble sort function

--- javascript/bubble-sort.js
+++ javascript/bubble-sort.js
@@ -3,3 +3,21 @@ The bubble sort is a sorting algorithm that iterates through each element of the
 array multple times, comparing two adjacent elements at a time and swapping their
 positions accordingly.
 */
+
+let bubbleSort = (numbers) => {
+  let swapped;
+  do {
+    swapped = false;
+    for (let i = 0; i < numbers.length; i++) {
+      if (numbers[i] > numbers[i+1]) {
+        let temp = numbers[i];
+        numbers[i] = numbers[i+1];
+        numbers[i+1] = temp;
+        swapped = true;
+      }
+    }
+  } while (swapped);
+  return numbers;
+}
+
+console.log(bubbleSort([5,2,0,1,3,1,4]));
