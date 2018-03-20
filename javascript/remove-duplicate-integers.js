0fd90df40b906304bc82cafc80eddc7abfbcad5a
Kay Mok <kaymok3@gmail.com>
2018-03-20 11:16:38 -0400

    Add solution without built-in methods

--- javascript/remove-duplicate-integers.js
+++ javascript/remove-duplicate-integers.js
@@ -1,11 +1,14 @@
 /*
 Given an array of integers, write a function that removes all duplicates from
 the array.
+
+- INPUT: removeDuplicates([1, 3, 5, 3, 7, 1])
+- OUTPUT: [1, 3, 5, 7]
 */
 
-function removeDuplicates(arr) {
+let removeDuplicates = (arr) => {
   let output = [];
-  for (var i = 0; i < arr.length; i++) {
+  for (let i = 0; i < arr.length; i++) {
     if (!output.includes(arr[i])) {
       output.push(arr[i])
     }
@@ -14,4 +17,18 @@ function removeDuplicates(arr) {
 }
 
 console.log(removeDuplicates([1, 3, 5, 3, 7, 1]));
-console.log(removeDuplicates([1, 20, 3, 10, 20, 3, 5, 3, 7, 1]));
+
+/* michael's solution changed to for loops */
+
+let removeD = (arr) => {
+  let arr2 = arr.slice();
+  let i = 0;
+  for (let i = 0; i < arr2.length; i++) {
+    for (let k = i + 1; k < arr2.length; k++) {
+      if (arr2[i] === arr2[k]) { arr2.splice(k, 1) }
+    }
+  }
+  return arr2;
+}
+
+console.log(removeD([1, 3, 5, 3, 7, 1]));
