9dff06a063ca3e0a70eeb23f9d0fa443e51327ba
Kay Mok <kaymok3@gmail.com>
2018-03-31 18:39:47 -0400

    Add instructions for function to create the smallest possible number by taking one digit and changing its index

---
+++ javascript/create-smallest-num.js
@@ -0,0 +1,15 @@
+/*
+Given a positive number, create the smallest possible number by taking one digit
+and changing its index.
+
+Return [smallest number, old index , new index]
+
+The indexes need to be as small as possible.
+
+**Example**
+
+- INPUT: smallest(261235);
+- OUTPUT: [126235, 2, 0]
+126235 is the smallest number gotten by taking 1 at index 2 and putting it at index 0
+
+*/
