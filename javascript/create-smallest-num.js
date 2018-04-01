d2c77198b3588fe7a5d6ff8356cb21c56ab5be65
Kay Mok <kaymok3@gmail.com>
2018-03-31 23:00:30 -0400

    Improve solution to pass more tests

--- javascript/create-smallest-num.js
+++ javascript/create-smallest-num.js
@@ -25,7 +25,7 @@ function smallest(n) {
   for (let i = 0; i < original.length; i++) {
     if (sorted[i] !== original[i]) {
       digitToMove = sorted[i];
-      indexFrom = original.indexOf(sorted[i]); // get index of where to remove digit
+      indexFrom = original.lastIndexOf(sorted[i]); // get index of where to remove digit
       break;
     }
   }
@@ -35,19 +35,34 @@ function smallest(n) {
   newNum.splice(indexFrom, 1); // remove digit from copy of original
   console.log(newNum);
   let indexTo;
-  for (let i = 0; i < newNum.length; i++) { // locate where to insert digit
-    if (newNum[i] <= digitToMove && newNum[i + 1] > digitToMove) {
-      indexTo = i + 1;
-      break;
-    } else {
-      indexTo = 0;
+  if (digitToMove === 0) {
+    indexTo = 0;
+  } else {
+    for (let i = 0; i < newNum.length; i++) { // locate where to insert digit
+      if (newNum[i] < digitToMove && newNum[i + 1] > digitToMove) {
+        indexTo = i + 1;
+        break;
+      } else if (newNum[i] === digitToMove && newNum[i + 1] > digitToMove) {
+        indexTo = i;
+      } else {
+        indexTo = 0;
+      }
     }
   }
   newNum.splice(indexTo, 0, digitToMove); // insert digit to copy of original
   console.log(newNum);
   console.log(indexTo);
-  output.push(Number(newNum.join("")), indexFrom, indexTo);
+  if (digitToMove === 0 && indexTo === 0 && indexFrom === 1) {
+    output.push(Number(newNum.join("")), indexTo, indexFrom);
+  } else {
+    output.push(Number(newNum.join("")), indexFrom, indexTo);
+  }
   return output;
 }
 
+console.log(smallest(261235));
+console.log(smallest(209917));
+console.log(smallest(285365));
+console.log(smallest(269045));
 console.log(smallest(296837));
+console.log(smallest(1000000));
