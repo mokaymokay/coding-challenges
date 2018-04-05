4211b1374b6d838e2dc674bda1a20b04caa4161b
Kay Mok <kaymok3@gmail.com>
2018-04-04 21:50:19 -0400

    Refactor

--- ruby/people_array.rb
+++ ruby/people_array.rb
@@ -2,42 +2,24 @@
 # people = [["Jason", "Maria", "Justin"], ["Red", "Blue", "Green"], ["Limp Bizkit", "3 Doors Down", "Papa Roach"]]
 # The sentence will look like "Jason's favorite color is Red, and he loves Limp Bizkit"
 
-names = []
-colors = []
-bands = []
+names, colors, bands = [], [], []
 
-puts "Enter three names"
-
-i = 0
-while i < 3 do
-  input = gets.chomp.capitalize
-  names.push(input)
-  i += 1
+def get_three_inputs(array, info_type)
+  puts "Enter three " + info_type
+  3.times do
+    input = gets.chomp
+    array.push(input)
+  end
 end
 
-puts "Enter three colors"
-
-i = 0
-while i < 3 do
-  input = gets.chomp
-  colors.push(input)
-  i += 1
-end
-
-puts "Enter three bands"
-
-i = 0
-while i < 3 do
-  input = gets.chomp.capitalize
-  bands.push(input)
-  i += 1
-end
+get_three_inputs(names, "names")
+get_three_inputs(colors, "colors")
+get_three_inputs(bands, "bands")
 
 people = []
 people.push(names, colors, bands)
 
-i = 0
-while i < 3 do
-  puts "#{names[i]}'s favorite color is #{colors[i]}, and s(he) loves #{bands[i]}"
-  i += 1
+
+for i in 0..people.length - 1
+  puts "#{people[0][i].capitalize}'s favorite color is #{people[1][i]}, and s/he loves #{people[2][i].capitalize}"
 end
