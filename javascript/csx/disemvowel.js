e1ecebf0adbbe58c227766371a97b8b4da0fbc9a
Kay Mok <kaymok3@gmail.com>
2018-06-25 17:17:59 -0400

    Refactor to use object instead of array to store vowels and reduce time complexity from O(n^2) to O(n)

--- javascript/csx/disemvowel.js
+++ javascript/csx/disemvowel.js
@@ -1,10 +1,25 @@
 // This function takes in a string and returns a new string with all vowels removed.
 
+// time complexity: O(n)
+// space complexity: O(n)
+// where n = length of string
+
 const disemvowel = str => {
-  let vowels = ["a", "e", "i", "o", "u", "A", "E", "I", "O", "U"]
+  let vowels = {
+    "a": true,
+    "e": true,
+    "i": true,
+    "o": true,
+    "u": true,
+    "A": true,
+    "E": true,
+    "I": true,
+    "O": true,
+    "U": true
+  }
   let result = ""
   for (let char of str) {
-    if (!vowels.includes(char)) result += char
+    if (!vowels[char]) result += char
   }
   return result
 }
