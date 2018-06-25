623a4c788be1f09f25f06940f85a1c5e9e29a784
Kay Mok <kaymok3@gmail.com>
2018-06-25 17:39:57 -0400

    Add function that returns an array using an input object of elements and counts

---
+++ javascript/csx/array-builder.js
@@ -0,0 +1,18 @@
+// This function takes in an object of words and counts, and returns an array with
+// words as elements by 'count' number of times. The order of the elements in the
+// array does not matter, but repeated elements should be grouped.
+
+const arrayBuilder = obj => {
+  let result = []
+  for (let word in obj) {
+    let count = obj[word]
+    while (count > 0) {
+      result.push(word)
+      count -= 1
+    }
+  }
+  return result
+}
+
+console.log(arrayBuilder({'cats': 2, 'dogs': 1})); //=> ['cats', 'cats', 'dogs']
+console.log(arrayBuilder({})); //=> []
