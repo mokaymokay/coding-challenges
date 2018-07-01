6f40df7b39fd55046d3fc2ff5ee2a9017789e477
Kay Mok <kaymok3@gmail.com>
2018-07-01 14:06:31 -0400

    Add function that accepts an input and returns a function that adds a value to that input and returns it

---
+++ javascript/csx/add-by-x.js
@@ -0,0 +1,18 @@
+// The function addByX returns a function that will add an input by x.
+
+const addByX = x => {
+  return value => value + x
+}
+
+const addByTwo = addByX(2);
+console.log(addByTwo(1)); //should return 3
+console.log(addByTwo(2)); //should return 4
+console.log(addByTwo(3)); //should return 5
+
+const addByThree = addByX(3);
+console.log(addByThree(1)); //should return 4
+console.log(addByThree(2)); //should return 5
+
+const addByFour = addByX(4);
+console.log(addByFour(4)); //should return 8
+console.log(addByFour(10)); //should return 14
