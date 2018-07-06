4118b9b225a7c9668d6193aa629cea4e4edf1276
Kay Mok <kaymok3@gmail.com>
2018-07-06 16:47:19 -0400

    Refactor countBy using reduce

--- javascript/csx/count-by.js
+++ javascript/csx/count-by.js
@@ -1,21 +1,20 @@
 // This function performs a callback on each element of an array and returns
 // an object of the results and their counts.
 
-const countBy = (arr, callback) => {
-  let count = {}
-  arr.forEach( (elem) => {
-    if (count[callback(elem)]) {
-      count[callback(elem)] += 1
+let countBy = (arr, callback) => {
+  return arr.reduce( (counted, elem) => {
+    if (callback(elem) in counted) {
+      counted[callback(elem)] += 1
     } else {
-      count[callback(elem)] = 1
+      counted[callback(elem)] = 1
     }
-  })
-  return count
+    return counted
+  }, {})
 }
 
 function evenOdd(n) {
   if (n % 2 === 0) return 'even';
   else return 'odd';
 }
-var nums = [1, 2, 3, 4, 5];
+let nums = [1, 2, 3, 4, 5];
 console.log(countBy(nums, evenOdd)); // should log: { odd: 3, even: 2 }
