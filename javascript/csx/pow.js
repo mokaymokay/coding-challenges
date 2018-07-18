1a551b424e4cace6739029ea6233ff15c2cd3b4c
Kay Mok <kaymok3@gmail.com>
2018-07-18 12:12:48 -0400

    Refactor

--- javascript/csx/pow.js
+++ javascript/csx/pow.js
@@ -1,12 +1,8 @@
 // This function takes a base and an exponent and returns base to the power of exponent.
 
 const pow = (base, exponent, output = 1) => {
-  if (exponent === 0) { return output }
-  while (exponent > 0) {
-    output *= base
-    exponent--
-    return pow(base, exponent, output)
-  }
+  if (exponent === 0) return output;
+  return pow(base, exponent - 1, output * base);
 }
 
 console.log(pow(2, 4)); // -> 16
