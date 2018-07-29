fa10a0cfccfafe75a94fd220fab140b447e5eaa4
Kay Mok <kaymok3@gmail.com>
2018-07-29 15:40:49 -0400

    Add subclass

--- javascript/csx/oop.js
+++ javascript/csx/oop.js
@@ -15,3 +15,25 @@ class PersonClass {
 const george = new PersonClass('George');
 
 george.greet(); // -> Logs 'hello'
+
+// Create a class DeveloperClass that creates objects by extending the PersonClass
+// class. In addition to having a name property and greet method, DeveloperClass
+// should have an introduce method. When called, introduce should log the string
+// "Hello World, my name is [name]".
+
+class DeveloperClass extends PersonClass {
+  constructor(name, age) {
+    super(name)
+    this.age = age
+  }
+
+  introduce() {
+    console.log("Hello World, my name is " + this.name)
+  }
+}
+
+const thai = new DeveloperClass('Thai', 32);
+
+console.log(thai.name); // -> Logs 'Thai'
+console.log(thai.age); // -> Logs 32
+thai.introduce(); // -> Logs 'Hello World, my name is Thai'
