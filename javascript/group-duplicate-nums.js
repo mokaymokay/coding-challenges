932f3c2193c32ae5de6f2229a706d1e6a67f6fde
Kay Mok <kaymok3@gmail.com>
2018-08-01 13:45:24 -0400

    Add solution for group duplicate numbers

--- javascript/group-duplicate-nums.js
+++ javascript/group-duplicate-nums.js
@@ -3,7 +3,17 @@
 // the number they contain.
 
 module.exports = {
-  groupDuplicateNums: function() {
-    
+  groupDuplicateNums: function(array) {
+    let output = []
+    if (array === []) { return output }
+    let count = {}
+    let order = [...new Set(array)]
+    for (let num of array) {
+      count[num] === undefined ? count[num] = 1 : count[num] += 1
+    }
+    for (let num of order) {
+      output.push(new Array(count[num]).fill(num))
+    }
+    return output
   }
 }
