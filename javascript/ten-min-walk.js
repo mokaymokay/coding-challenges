de24759479c9754b03d81f71617e503b18c47709
Kay Mok <kaymok3@gmail.com>
2018-08-03 18:55:57 -0400

    Add solution for ten minute walk problem

---
+++ javascript/ten-min-walk.js
@@ -0,0 +1,16 @@
+// This program, given an array of directions for a walk (eg. ['n', 's', 'w', 'e']), determines whether
+// (1) the walk is exactly ten minutes long and (2) takes you to back the starting point.
+
+// Time complexity: O(n)
+// Space complexity: O(n)
+
+module.exports = {
+  isVaildWalk: function(directions) {
+    if (directions.length !== 10) return false
+    const count = {}
+    for (let direction of directions) {
+      count[direction] === undefined? count[direction] = 1 : count[direction] += 1
+    }
+    return count['n'] === count['s'] && count['w'] === count['e']
+  }
+}
