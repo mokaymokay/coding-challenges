e3e2af67b9abe58b1dede5145ee4c91bcc4b23ea
Kay Mok <kaymok3@gmail.com>
2018-08-03 17:40:44 -0400

    Add test for ten minute walk problem

---
+++ javascript/test/ten-min-walk-test.js
@@ -0,0 +1,33 @@
+const assert = require('assert')
+const tenMinWalk = require('../ten-min-walk')
+const isVaildWalk = tenMinWalk.isVaildWalk
+
+describe('Ten Minute Walk', function() {
+  describe('Route one', function() {
+    it('should return true', function() {
+      actual = isVaildWalk(['n', 's', 'n', 's', 'n', 's', 'n', 's', 'n', 's'])
+      assert.equal(actual, true)
+    })
+  })
+
+  describe('Route two', function () {
+    it('should return false', function () {
+      actual = isVaildWalk(['w', 'e', 'w', 'e', 'w', 'e', 'w', 'e', 'w', 'e', 'w', 'e'])
+      assert.equal(actual, false)
+    })
+  })
+
+  describe('Route three', function () {
+    it('should return false', function () {
+      actual = isVaildWalk(['w'])
+      assert.equal(actual, false)
+    })
+  })
+
+  describe('Route four', function () {
+    it('should return false', function () {
+      actual = isVaildWalk(['n', 'n', 'n', 's', 'n', 's', 'n', 's', 'n', 's'])
+      assert.equal(actual, false)
+    })
+  })
+})
